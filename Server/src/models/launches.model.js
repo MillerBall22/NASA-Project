367ab09e6f64196ebc9c605576f6d65056f157fc
Miller_Ball_22 <millerball22@gmail.com>
2022-06-06 08:44:03 -0600

    June 6

---
+++ Server/src/models/launches.model.js
@@ -0,0 +1,88 @@
+const { updateOne } = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
+const DEFAULT_FLIGHT_NUMBER = 100;
+
+const launch = {
+    flightNumber: 100,
+    mission: "Kepler Exploration X",
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    target: "Kepler-442 b",
+    customer: ["ZTM", "NASA"],
+    upcoming: true,
+    success: true
+};
+
+//saveLaunch(launch);
+
+async function existsLaunchWithId(launchId) {
+    return await launchesDatabase.findOne({
+        flightNumber: launchId
+    });
+}
+
+async function scheduleNewLaunch(launch) {
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+
+    const newLaunch = Object.assign(launch, {
+        flightNumber: newFlightNumber,
+        customer: ["ZTM", "NASA"],
+        upcoming: true,
+        success: true,
+    })
+
+    await saveLaunch(newLaunch);
+};
+
+async function saveLaunch(launch) {
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    })
+
+    if (!planet) {
+        throw new Error('No matching planet was found');
+    }
+
+    await launchesDatabase.findOneAndUpdate({
+        flightNumber: launch.flightNumber
+    }, launch, {
+        upsert: true,
+    })
+}
+
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase
+        .findOne({})
+        .sort('-flightNumber');
+
+    if (!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER;
+    }
+
+    return latestLaunch.flightNumber;
+}
+
+async function getAllLaunches() {
+    return await launchesDatabase
+        .find({}, { '_id': 0, '__v': 0 })
+}
+
+async function abortLaunchById(launchId) {
+    const aborted = await launchesDatabase.updateOne({
+        flightNumber: launchId
+    }, {
+        upcoming: false,
+        success: false,
+    })
+
+    return aborted.modifiedCount === 1;
+}
+
+module.exports = {
+    getAllLaunches,
+    scheduleNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById
+};
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,19 +1,22 @@
 const http = require('http');
 
-const app = require('./app')
+const app = require('./app');
 
-const {loadPlanetsData} = require('./models/planets.model')
+const { mongoConnect } = require('./services/mongo');
+
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 async function startServer() {
-  await loadPlanetsData();
- 
-  server.listen(PORT, () => {
-    console.log(`Server is listening on PORT:${PORT}`);
-  });
+    await mongoConnect();
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Server is listening on PORT:${PORT}`);
+    });
 }
 
 startServer();
